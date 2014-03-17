ef462680bf5802819e653fb9b7806d9be56460aa
Michael Holroyd <meekohi@gmail.com>
2014-03-17 13:04:30 -0400

    whitespace

--- lib/pipedrive/search-result.rb
+++ lib/pipedrive/search-result.rb
@@ -3,7 +3,7 @@ module Pipedrive
 
     # Class Methods
     class << self
-      
+
       def search(term, start=0, limit=nil)
         res = get(resource_path, :query => { :term => term, :start => start, :limit => limit})
         if res.ok?
@@ -12,7 +12,7 @@ module Pipedrive
           bad_response(res,{:term=>term,:start=>start,:limit=>limit})
         end
       end
-  
+
       def field(term, field_type, field_key, opts={})
         res = get("#{resource_path}/field", :query => opts.merge(:term => term, :field_type => field_type, :field_key => field_key) )
         if res.ok?
@@ -21,8 +21,8 @@ module Pipedrive
           bad_response(res,{:term=>term,:field_type=>field_type,:field_key=>field_key}.merge(opts))
         end
       end
-    
+
     end
-    
+
   end
 end
